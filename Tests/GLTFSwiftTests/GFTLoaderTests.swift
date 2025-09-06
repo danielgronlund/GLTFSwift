4ab0380b304b528b48c9f06535fadaa712b19eab
Daniel Grönlund <just.danne@gmail.com>
2025-09-06 20:53:26 +0200

    Expose interface for loading with file URL Use fileURL internally instead of passing Bundle

--- Tests/GLTFSwiftTests/GFTLoaderTests.swift
+++ Tests/GLTFSwiftTests/GFTLoaderTests.swift
@@ -12,24 +12,40 @@ final class FileLoadingTests: XCTestCase {
   }
 
   func testLoadFile() throws {
-    _ = try loader.loadContainer(path: "simple-cube.gltf", in: .module)
+    let url = try given(resource: "simple-cube.gltf", in: .module)
+    _ = try loader.loadContainer(from: url)
   }
 
   func testLoadFileWithJoints() throws {
-    _ = try loader.loadContainer(path: "simple-cube-joints.gltf", in: .module)
+    let url = try given(resource: "simple-cube-joints.gltf", in: .module)
+    _ = try loader.loadContainer(from: url)
   }
 
   func testLoadCompressedModel() throws {
-    _ = try loader.loadContainer(path: "draco-compressed.gltf", in: .module)
+    let url = try given(resource: "draco-compressed.gltf", in: .module)
+
+    _ = try loader.loadContainer(from: url)
   }
 
   func testLoadUV() throws {
-    _ = try loader.loadContainer(path: "uv_cube.gltf", in: .module)
+    let url = try given(resource: "uv_cube.gltf", in: .module)
+
+    _ = try loader.loadContainer(from: url)
     // TODO: Verify UV Coordinates
   }
 
   func testLoadUVCompressed() throws {
-    _ = try loader.loadContainer(path: "uv_cube_compressed.gltf", in: .module)
+    let url = try given(resource: "uv_cube_compressed.gltf", in: .module)
+    _ = try loader.loadContainer(from: url)
     // TODO: Verify UV Coordinates
+  }
+
+  func given(resource: String, in bundle: Bundle, file: StaticString = #file, line: UInt =  #line) throws -> URL {
+    let url = bundle.url(forResource: resource, withExtension: nil)
+    return try given(url: url, file: file, line: line)
+  }
+
+  func given(url: URL?, file: StaticString = #file, line: UInt = #line) throws -> URL {
+    try XCTUnwrap(url, file: file, line: line)
   }
 }
